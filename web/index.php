7c9a222849d720d01c7dda7fb84481472afd23b2
vvpanchev <vladislav_3@abv.bg>
2022-03-13 13:24:32 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,17 +6,12 @@
   <body>
     <div align="center">
       <h1>Факти за България</h1>
-      <img src="bulgaria-map.png" />
+      <img src="bulgaria-map.jpg" />
       <table>
         <tr>
           <td>Площ</td>
           <td></td>
           <td>110 993.6 кв.км.</td>
-        </tr>
-        <tr>
-          <td>Население</td>
-          <td></td>
-          <td>7 101 859</td>
         </tr>
         <tr>
           <td>Столица</td>
@@ -25,21 +20,26 @@
         </tr>
       </table>
       <br />
-      <h1>Големи градове</h1>
+      <h1>НАСЕЛЕНИЕ КЪМ 31.12.2020 Г. ПО ОБЛАСТИ И ПОЛ</h1>
       <table>
 <?php
    require_once ('config.php');
 
    try {
       $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
-      $query = $connection->query("SELECT city_name, population FROM cities ORDER BY population DESC");
+      $query = $connection->query("SELECT region_name, men ,women FROM region ORDER BY region_name ASC");
       $cities = $query->fetchAll();
 
       if (empty($cities)) {
          echo "<tr><td>Няма данни.</td></tr>\n";
       } else {
          foreach ($cities as $city) {
-            print "<tr><td>{$city['city_name']}</td><td align=\"right\">{$city['population']}</td></tr>\n";
+            print "<tr>\n";
+            print "<td>{$city['region_name']}</td>\n";
+            print "<td>{$city['men']}</td>\n";
+            print "<td>{$city['women']}</td>\n";
+            print "<td>{$city['men'] + $city['women']}</td>\n";
+            print "</tr>\n";
          }
       }
    }
